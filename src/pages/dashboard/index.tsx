d46b8a3f12a909072b7585c77dfb5c5182476ff2
jinmin.kim <bmpr14@gmail.com>
2024-09-29 16:38:58 +0900

    feat: add tabs component with switching functionality

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,25 @@
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/shared/ui/shadcn/tabs";
 import { GroupManagement } from "@/widgets/GroupManagement";
 
 function DashboardPage() {
   return (
-    <div className="h-screen bg-gradient-to-t from-yellow-50 to-blue-600">
+    <div className="h-screen bg-gradient-to-t from-blue-600 to-yellow-50 dark:to-blue-600 dark:from-yellow-50">
       <div className="container p-4 mx-auto">
-        <GroupManagement />
+        <Tabs defaultValue="menuA">
+          <TabsList>
+            <TabsTrigger value="menuA">Menu A</TabsTrigger>
+            <TabsTrigger value="menuB">Menu B</TabsTrigger>
+          </TabsList>
+          <TabsContent value="menuA">
+            <GroupManagement />
+          </TabsContent>
+          <TabsContent value="menuB">...</TabsContent>
+        </Tabs>
       </div>
     </div>
   );
